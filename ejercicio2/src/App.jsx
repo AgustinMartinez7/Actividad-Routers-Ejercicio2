942fdc0921ab5e7ba70060dd3d5dcf71c110aebf
AgustinMartinez7 <agustindago100@gmail.com>
2025-10-16 08:39:19 -0300

    Corrige errores

--- ejercicio2/src/App.jsx
+++ ejercicio2/src/App.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import Register from "./components/register/Register";
-import PersonalInfo from "./components/register/PersonalInfo";
-import ContactInfo from "./components/register/ContactInfo";
-import Confirmation from "./components/register/Confirmation";
+/* import Home from "./components/Home"; */
+import Register from "./components/Register";
+import PersonalInfo from "./components/PersonalInfo";
+import ContactInfo from "./components/ContactInfo";
+import Confirmation from "./components/Confirmation";
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <nav style={{ padding: "20px", borderBottom: "1px solid #ccc" }}>
-          <Link to="/" style={{ marginRight: "15px" }}>
-            Inicio
-          </Link>
-          <Link to="/register">Registrarse</Link>
+          <Link to="/" style={{ marginRight: "15px" }}>Inicio</Link>
+          <Link to="/register/personal"  >Registrarse</Link>
+
+          
+          
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />}>
+          {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/register" element={<Register />}>
             <Route path="personal" element={<PersonalInfo />} />
             <Route path="contact" element={<ContactInfo />} />
             <Route path="confirmation" element={<Confirmation />} />
